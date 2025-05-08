8ea6336c61d59e636d5f90ba85145ebc4cd37f0f
RflPtrk <rafalko.piotr.1@gmail.com>
2025-05-08 10:09:13 +0200

    Added docstrings to utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
+"""Simple math utility functions."""
+
 def add (a: int, b: int) -> int:
+    """Returns the sum of a and b."""
     return a + b
 
 def substract (a: int, b: int) -> int:
+    """Returns the substraction result of a and b."""
     return a - b
 
 def multiply(a: int, b: int) -> int:
+    """Returns the product of a and b."""
     return a * b
 
 def divide(a: int, b: int) -> int:
+    """Returns the quotient of a and b."""
     return a / b
